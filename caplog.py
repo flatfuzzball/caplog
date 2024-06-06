d01a44e162d738076d2c7467f29479e58c6d8d02
flat <127520563+flatfuzzball@users.noreply.github.com>
2024-06-06 19:57:22 +0200

    Update caplog.py

--- caplog.py
+++ caplog.py
@@ -1,17 +1,18 @@
-#! /usr/bin/python
-
-from os import listdir, path
+#! /usr/bin/python3
+import subprocess
+from os import listdir, path      
 from datetime import datetime
 
 # personal settings, you'll wanna change these.
-title = "boblog"
-desc = "bobs awesome gemlog!"
+title = "flat's gemlog"
+desc = "probably not so up to date."
 
-# blog/technical settings, ignore tells the program which .gmi files to ignore. gemfeed controls whether or not to use gemfeed
+# blog/technical settings, ignore tells the program which .gmi files to ignore.
 timeformat = "%Y-%m-%d"
 ignore = ["index.gmi",]
-gemfeed = True
+gemfeed = "True"
 gemfeed_path = "../gemfeed.py"
+url = "gemini://tilde.team/~flat"
 
 f = open("index.gmi", "w")
 f.write(f"# {title}\n## {desc}\n")
@@ -20,5 +21,8 @@ f.write(f"# {title}\n## {desc}\n")
 for post in listdir():
     if post[-4:] == ".gmi" and post not in ignore:
         f.write(f"\n=> {post} {datetime.fromtimestamp(path.getmtime(post)).strftime(timeformat)} - {post[:-4]}")
-
+    
 f.close()
+
+if gemfeed:
+    subprocess.call(["python", gemfeed_path, "-b", url])
